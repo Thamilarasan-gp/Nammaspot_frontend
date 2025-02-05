9751b8ab879e32c64845aa6d91d803ee321c0fe5
Thamilarasan <thamilarasan.gp2023it@sece.ac.in>
2025-02-05 14:26:04 +0530

    error solved for leaflet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./USER1/Login/Login";
